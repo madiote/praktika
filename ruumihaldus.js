65eaa28a1c7598bb892879d6cea50565b16686c4
Karen <43170840+karenminski@users.noreply.github.com>
2019-06-11 14:32:00 +0300

    Koridorid visuaalselt lisatud ilma funktsioonideta

--- ruumihaldus.js
+++ ruumihaldus.js
@@ -11,10 +11,10 @@ $(document).one('pageinit', function () {
   $('#deleteButton').on('tap', deleteAll);
   $('#uploadButton').on('change', showFile);
 
-
   function myfunction() {
     console.log(this.fileContent);
   }
+
   /*  NÄITA FAILI */
 
   function showFile() {
@@ -24,6 +24,7 @@ $(document).one('pageinit', function () {
 
     if (file) {
       reader.onload = function (event) {
+        //let fileContent = event.target.result;
         let file = event.target.result;
         let allLines = file.split(/\r\n|\n/);
 
@@ -36,7 +37,6 @@ $(document).one('pageinit', function () {
             let purpose = eachElement[3];
             let seats = eachElement[4];
             let comments = eachElement[5];
-
 
             let property = {
               coordinates: coordinates,
@@ -71,6 +71,7 @@ $(document).one('pageinit', function () {
   }
 
   function redirect() {
+    // muuda nii et laed alla kogu info mis on kuvatud nagu ta oli selles todo kodutöös
     let classCoordinates;
     let classRoom;
     let classPeople;
@@ -86,11 +87,11 @@ $(document).one('pageinit', function () {
     today = dd + '/' + mm + '/' + yyyy;
 
     if (properties != "" && properties != null) {
+
       for (let i = 0; i < properties.length; i++) {
         let p = properties[i];
         data += String(p.coordinates) + "; " + String(p.room) + "; " + String(p.people) + "; " + String(p.purpose) +
           "; " + String(p.seats) + "; " + String(p.comments) + "; " + "\n";
-        console.log(p.coordinates.split("&"));
       }
     } else {
       console.log("Tühi");
@@ -106,27 +107,27 @@ $(document).one('pageinit', function () {
   /* KUSTUTA */
 
   function deleteAll() {
-    if (confirm("Kas oled kindel, et soovid kõik ruumid kustutada?\n(Enne kustutamist soovitame alla laadida hetke ruumid!)") == true) {
+    if (confirm("Kas oled kindel, et soovid kõik kustutada?\n(Enne kustutamist soovitame alla laadida hetke ruumid!)") == true) {
       localStorage.clear();
       window.location.href = "ruumihaldus.php";
     }
   }
 
   function deleteProperties() {
-    let l = localStorage;
-    l.setItem('currentCoordinates', $(this).data('coordinates'));
-    l.setItem('currentRoom', $(this).data('room'));
-    l.setItem('currentPeople', $(this).data('people'));
-    l.setItem('currentPurpose', $(this).data('purpose'));
-    l.setItem('currentSeats', $(this).data('seats'));
-    l.setItem('currentComments', $(this).data('comments'));
+    localStorage.setItem('currentCoordinates', $(this).data('coordinates'));
+    localStorage.setItem('currentRoom', $(this).data('room'));
+    localStorage.setItem('currentPeople', $(this).data('people'));
+    localStorage.setItem('currentPurpose', $(this).data('purpose'));
+    localStorage.setItem('currentSeats', $(this).data('seats'));
+    localStorage.setItem('currentComments', $(this).data('comments'));
 
-    let currentCoordinates = l.getItem('currentCoordinates');
-    let currentRoom = l.getItem('currentRoom');
-    let currentPurpose = l.getItem('currentPurpose');
-    let currentPeople = l.getItem('currentPeople');
-    let currentSeats = l.getItem('currentSeats');
-    let currentComments = l.getItem('currentComments');
+
+    let currentCoordinates = localStorage.getItem('currentCoordinates');
+    let currentRoom = localStorage.getItem('currentRoom');
+    let currentPurpose = localStorage.getItem('currentPurpose');
+    let currentPeople = localStorage.getItem('currentPeople');
+    let currentSeats = localStorage.getItem('currentSeats');
+    let currentComments = localStorage.getItem('currentComments');
 
     for (let i = 0; i < properties.length; i++) {
       let p = properties[i];
@@ -146,13 +147,12 @@ $(document).one('pageinit', function () {
   /* MUUDA */
 
   function editProperties() {
-    let l = localStorage;
-    let currentCoordinates = l.getItem('currentCoordinates');
-    let currentRoom = l.getItem('currentRoom');
-    let currentPurpose = l.getItem('currentPurpose');
-    let currentPeople = l.getItem('currentPeople');
-    let currentSeats = l.getItem('currentSeats');
-    let currentComments = l.getItem('currentComments');
+    let currentCoordinates = localStorage.getItem('currentCoordinates');
+    let currentRoom = localStorage.getItem('currentRoom');
+    let currentPurpose = localStorage.getItem('currentPurpose');
+    let currentPeople = localStorage.getItem('currentPeople');
+    let currentSeats = localStorage.getItem('currentSeats');
+    let currentComments = localStorage.getItem('currentComments');
 
     for (let i = 0; i < properties.length; i++) {
       let p = properties[i];
@@ -184,20 +184,19 @@ $(document).one('pageinit', function () {
   }
 
   function setCurrent() {
-    let l = localStorage;
-    l.setItem('currentCoordinates', $(this).data('coordinates'));
-    l.setItem('currentRoom', $(this).data('room'));
-    l.setItem('currentPeople', $(this).data('people'));
-    l.setItem('currentPurpose', $(this).data('purpose'));
-    l.setItem('currentSeats', $(this).data('seats'));
-    l.setItem('currentComments', $(this).data('comments'));
+    localStorage.setItem('currentCoordinates', $(this).data('coordinates'));
+    localStorage.setItem('currentRoom', $(this).data('room'));
+    localStorage.setItem('currentPeople', $(this).data('people'));
+    localStorage.setItem('currentPurpose', $(this).data('purpose'));
+    localStorage.setItem('currentSeats', $(this).data('seats'));
+    localStorage.setItem('currentComments', $(this).data('comments'));
 
-    $('#editCoordinates').val(l.getItem('currentCoordinates'));
-    $('#editRoom').val(l.getItem('currentRoom'));
-    $('#editPeople').val(l.getItem('currentPeople'));
-    $('#editPurpose').val(l.getItem('currentPurpose'));
-    $('#editSeats').val(l.getItem('currentSeats'));
-    $('#editComments').val(l.getItem('currentComments'));
+    $('#editCoordinates').val(localStorage.getItem('currentCoordinates'));
+    $('#editRoom').val(localStorage.getItem('currentRoom'));
+    $('#editPeople').val(localStorage.getItem('currentPeople'));
+    $('#editPurpose').val(localStorage.getItem('currentPurpose'));
+    $('#editSeats').val(localStorage.getItem('currentSeats'));
+    $('#editComments').val(localStorage.getItem('currentComments'));
 
 
   }
@@ -211,7 +210,6 @@ $(document).one('pageinit', function () {
     let purpose = $('#addClassPurpose').val();
     let seats = $('#addClassSeats').val();
     let comments = $('#addClassComments').val();
-
 
     let property = {
       coordinates: coordinates,
@@ -270,4 +268,8 @@ $(document).one('pageinit', function () {
       }
     }
   }
+
+  /* KORIDOR */ 
+
+
 });
--- ruumihaldus.php
+++ ruumihaldus.php
@@ -1,151 +1,247 @@
-<?php
+    <?php
   require("functions.php");
   //kui pole sisselogitud
   if(!isset($_SESSION["userId"])){
-    session_destroy();
 	  header("Location: login.php");
 	  exit();
   }
   //väljalogimine
   if(isset($_GET["logout"])) {
-    unset($_SESSION["userId"]);
-    session_destroy();
-	  header("Location: login.php?uuenda=jah");
+	  session_destroy();
+	  header("Location:login.php");
 	  exit();
   }
 ?>
+    <!DOCTYPE html>
+    <html lang="en">
 
-<!DOCTYPE html>
-<html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-<head>
-  <meta charset="UTF-8">
+      <link rel="stylesheet"
+        href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+      <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.css">
+      <link rel="stylesheet" href="tlu.css" />
 
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+      <script src="ruumihaldus.js"></script>
+      <title>DTI Ruumihaldus</title>
+    </head>
 
-  <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
-  <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.css">
-  <link rel="stylesheet" href="tlu.css">
+    <body>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js" defer></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js" defer></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js" defer></script>
-  <script src="ruumihaldus.js" defer></script>
-  <title>DTI Ruumihaldus</title>
-</head>
+      <!-- RUUMID -->
 
-<body>
-  <!-- HOME PAGE -->
-  <div data-role="page" id="home">
-    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header"
-      role="banner">
-      <h1 class="ui-title" tabindex="0" role="heading" aria-level="1">DTI Ruumihaldus</h1>
-    </div>
-    <div data-role="navbar">
-      <ul>
-        <li><a href="#home" data-transition="none" data-icon="home">Avaleht</a></li>
-        <li><a href="#add" data-transition="none" data-icon="plus">Lisa</a></li>
-      </ul>
-    </div>
-    <div data-role="content">
-      <ul id="properties" data-role="listview" data-filter="true" data-filter-placeholder="Otsi ruumi..."
-        data-inset="true"></ul>
-      <button id="downloadButton" data-theme="A">Lae alla</button>
-      <button id="deleteButton" data-theme="A" onclick="deleteAll()">Kustuta kõik andmed</button>
-      <div>
-        <label for="uploadButton" class="buttonLabel">Lae fail üles, et sealt andmed lehele lugeda:</label>
-        <input type="file" data-theme="A" id="uploadButton">
+      <div data-role="page" id="rooms">
+        <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header"
+          role="banner">
+          <h1 class="ui-title" tabindex="0" role="heading" aria-level="1">DTI Ruumihaldus</h1>
+        </div>
+        <div data-role="navbar">
+          <ul>
+            <li><a href="#rooms" data-transition="none" data-icon="bars">Ruumid</a></li>
+            <li><a href="#coridors" data-transition="none" data-icon="bars">Koridorid</a></li>
+            <li><a href="#addRoom" data-transition="none" data-icon="plus">Lisa ruum</a></li>
+            <li><a href="#addCoridor" data-transition="none" data-icon="plus">Lisa koridor</a></li>
+          </ul>
+        </div>
+        <div data-role="content">
+          <ul id="properties" data-role="listview" data-filter="true" data-filter-placeholder="Otsi ruumi..."
+            data-inset="true"></ul>
+
+          <button id="downloadButton" data-theme="A">Lae alla</button>
+          <button id="deleteButton" data-theme="A" onclick="deleteAll()">Kustuta kõik andmed</button>
+          <div>
+            <label for="uploadButton" class="buttonLabel">Lae fail üles, et sealt andmed lehele lugeda:</label>
+            <input type="file" data-theme="A" id="uploadButton">
+          </div>
+        </div>
+        <div class="ui-footer ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="footer"
+          role="banner">
+          <h1 class="ui-title" tabindex="0" role="heading" aria-level="1">DTI Ruumihaldus</h1>
+        </div>
       </div>
-    </div>
-    <div class="ui-footer ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="footer"
-      role="banner">
-      <h1 class="ui-title" tabindex="0" role="heading" aria-level="1">DTI Ruumihaldus</h1>
 
-    </div>
-    <b><a href="?logout=1" onclick="return reload();">Logi välja</a></b>
-    <script type="text/javascript">
-      function reload() {
-        setTimeout(function () {
-          location.reload();
-        }, 10);
-      }
-    </script>
-  </div>
-  <!-- ADD PAGE -->
-  <div data-role="page" id="add">
-    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header"
-      role="banner">
-      <h1 class="ui-title" tabindex="0" role="heading" aria-level="1">DTI Ruumihaldus</h1>
-    </div>
-    <div data-role="navbar">
-      <ul>
-        <li><a href="#home" data-transition="none" data-icon="home">Avaleht</a></li>
-        <li><a href="#add" data-transition="none" data-icon="plus">Lisa</a></li>
-      </ul>
-    </div>
-    <div data-role="content">
-      <form id="addForm">
-        <label for="addClassCoordinates">Sisesta ruumi koordinaadid: </label>
-        <input type="text" id="addClassCoordinates">
+      <!-- KORIDORID -->
 
-        <label for="addClassRoom">Sisesta ruumi nimi: </label>
-        <input type="text" id="addClassRoom">
+      <div data-role="page" id="coridors">
+        <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header"
+          role="banner">
+          <h1 class="ui-title" tabindex="0" role="heading" aria-level="1">DTI Ruumihaldus</h1>
+        </div>
+        <div data-role="navbar">
+          <ul>
+            <li><a href="#rooms" data-transition="none" data-icon="bars">Ruumid</a></li>
+            <li><a href="#coridors" data-transition="none" data-icon="bars">Koridorid</a></li>
+            <li><a href="#addRoom" data-transition="none" data-icon="plus">Lisa ruum</a></li>
+            <li><a href="#addCoridor" data-transition="none" data-icon="plus">Lisa koridor</a></li>
+          </ul>
+        </div>
+        <div data-role="content">
+          <ul id="properties" data-role="listview" data-filter="true" data-filter-placeholder="Otsi ruumi..."
+            data-inset="true"></ul>
 
-        <label for="addClassPeople">Sisesta ruumiga seotud isikud: </label>
-        <input type="text" id="addClassPeople">
+          <button id="downloadButton" data-theme="A">Lae alla</button>
+          <button id="deleteButton" data-theme="A" onclick="deleteAll()">Kustuta kõik andmed</button>
+          <div>
+            <label for="uploadButton" class="buttonLabel">Lae fail üles, et sealt andmed lehele lugeda:</label>
+            <input type="file" data-theme="A" id="uploadButton">
+          </div>
+        </div>
+        <div class="ui-footer ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="footer"
+          role="banner">
+          <h1 class="ui-title" tabindex="0" role="heading" aria-level="1">DTI Ruumihaldus</h1>
+        </div>
+      </div>
 
-        <label for="addClassPurpose">Sisesta ruumi eesmärk: </label>
-        <input type="text" id="addClassPurpose">
+      <!-- ADD ROOM PAGE -->
+      <div data-role="page" id="addRoom">
+        <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header"
+          role="banner">
+          <h1 class="ui-title" tabindex="0" role="heading" aria-level="1">DTI Ruumihaldus</h1>
+        </div>
+        <div data-role="navbar">
+          <ul>
+            <li><a href="#rooms" data-transition="none" data-icon="bars">Ruumid</a></li>
+            <li><a href="#coridors" data-transition="none" data-icon="bars">Koridorid</a></li>
+            <li><a href="#addRoom" data-transition="none" data-icon="plus">Lisa ruum</a></li>
+            <li><a href="#addCoridor" data-transition="none" data-icon="plus">Lisa koridor</a></li>
+          </ul>
+        </div>
+        <div data-role="content">
+          <form id="addForm">
+            <label for="addClassCoordinates">Sisesta ruumi koordinaadid: </label>
+            <input type="text" id="addClassCoordinates">
 
-        <label for="addClassSeats">Sisesta ruumi kohtade arv: </label>
-        <input type="number" id="addClassSeats" min="1" max="500">
+            <label for="addClassRoom">Sisesta ruumi nimi: </label>
+            <input type="text" id="addClassRoom">
 
-        <label for="addClassComments">Lisa kommentaare: </label>
-        <input type="text" id="addClassComments">
+            <label for="addClassPeople">Sisesta ruumiga seotud isikud: </label>
+            <input type="text" id="addClassPeople">
 
-        <button id="submitAdd" class="ui-btn ui-corner-all">LISA</button>
-      </form>
-    </div>
-    <div class="ui-footer ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="footer"
-      role="banner">
-      <h1 class="ui-title" tabindex="0" role="heading" aria-level="1">DTI Ruumihaldus</h1>
-    </div>
-  </div>
-  <!-- EDIT PAGE -->
-  <div data-role="page" id="edit">
-    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header"
-      role="banner">
-      <h1 class="ui-title" tabindex="0" role="heading" aria-level="1">DTI Ruumihaldus</h1>
-    </div>
-    <div data-role="navbar">
-      <ul>
-        <li><a href="#home" data-transition="none" data-icon="home">Avaleht</a></li>
-        <li><a href="#add" data-transition="none" data-icon="plus">Lisa</a></li>
-      </ul>
-    </div>
-    <div data-role="content">
-      <form id="editForm">
-        <label for="editCoordinates">Sisesta ruumi koordinaadid: </label>
-        <input type="text" id="editCoordinates">
-        <label for="editRoom">Sisesta ruumi nimi: </label>
-        <input type="text" id="editRoom">
-        <label for="editPeople">Sisesta ruumi eesmärk:</label>
-        <input type="text" id="editPeople">
-        <label for="editPurpose">Sisesta ruumiga seotud inimesed:</label>
-        <input type="text" id="editPurpose">
-        <label for="editSeats">Sisesta ruumi kohtade arv: </label>
-        <input type="number" id="editSeats" min="1" max="500">
-        <label for="editComments">Lisa kommentaare: </label>
-        <input type="text" id="editComments">
-        <button id="submitEdit" class="ui-btn ui-corner-all">MUUDA</button>
-      </form>
-    </div>
-    <div class="ui-footer ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="footer"
-      role="banner">
-      <h1 class="ui-title" tabindex="0" role="heading" aria-level="1">DTI Ruumihaldus</h1>
-    </div>
-  </div>
-</body>
+            <label for="addClassPurpose">Sisesta ruumi eesmärk: </label>
+            <input type="text" id="addClassPurpose">
 
-</html>
+            <label for="addClassSeats">Sisesta ruumi kohtade arv: </label>
+            <input type="number" id="addClassSeats" min="1" max="500">
+
+            <label for="addClassComments">Lisa kommentaare: </label>
+            <input type="text" id="addClassComments">
+
+            <button id="submitAdd" class="ui-btn ui-corner-all">LISA</button>
+          </form>
+        </div>
+        <div class="ui-footer ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="footer"
+          role="banner">
+          <h1 class="ui-title" tabindex="0" role="heading" aria-level="1">DTI Ruumihaldus</h1>
+        </div>
+      </div>
+
+      <!-- ADD CORIDOR PAGE -->
+      <div data-role="page" id="addCoridor">
+        <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header"
+          role="banner">
+          <h1 class="ui-title" tabindex="0" role="heading" aria-level="1">DTI Ruumihaldus</h1>
+        </div>
+        <div data-role="navbar">
+          <ul>
+            <li><a href="#rooms" data-transition="none" data-icon="bars">Ruumid</a></li>
+            <li><a href="#coridors" data-transition="none" data-icon="bars">Koridorid</a></li>
+            <li><a href="#addRoom" data-transition="none" data-icon="plus">Lisa ruum</a></li>
+            <li><a href="#addCoridor" data-transition="none" data-icon="plus">Lisa koridor</a></li>
+          </ul>
+        </div>
+        <div data-role="content">
+          <form id="addForm">
+            <label for="addCoridorCoordinates">Sisesta koridori koordinaadid: </label>
+            <input type="text" id="addCoridorCoordinates">
+
+            <label for="addCoridorFloor">Sisesta korrus: </label>
+            <input type="text" id="addCoridorFloor">
+
+            <button id="submitAdd" class="ui-btn ui-corner-all">LISA</button>
+          </form>
+        </div>
+        <div class="ui-footer ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="footer"
+          role="banner">
+          <h1 class="ui-title" tabindex="0" role="heading" aria-level="1">DTI Ruumihaldus</h1>
+        </div>
+      </div>
+
+      <!-- EDIT ROOM PAGE -->
+
+      <div data-role="page" id="editRoom">
+        <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header"
+          role="banner">
+          <h1 class="ui-title" tabindex="0" role="heading" aria-level="1">DTI Ruumihaldus</h1>
+        </div>
+        <div data-role="navbar">
+          <ul>
+            <li><a href="#rooms" data-transition="none" data-icon="bars">Ruumid</a></li>
+            <li><a href="#coridors" data-transition="none" data-icon="bars">Koridorid</a></li>
+            <li><a href="#addRoom" data-transition="none" data-icon="plus">Lisa ruum</a></li>
+            <li><a href="#addCoridor" data-transition="none" data-icon="plus">Lisa koridor</a></li>
+          </ul>
+        </div>
+        <div data-role="content">
+          <form id="editForm">
+            <label for="editCoordinates">Sisesta ruumi koordinaadid: </label>
+            <input type="text" id="editCoordinates">
+            <label for="editRoom">Sisesta ruumi nimi: </label>
+            <input type="text" id="editRoom">
+            <label for="editPeople">Sisesta ruumi eesmärk:</label>
+            <input type="text" id="editPeople">
+            <label for="editPurpose">Sisesta ruumiga seotud inimesed:</label>
+            <input type="text" id="editPurpose">
+            <label for="editSeats">Sisesta ruumi kohtade arv: </label>
+            <input type="number" id="editSeats" min="1" max="500">
+            <label for="editComments">Lisa kommentaare: </label>
+            <input type="text" id="editComments">
+
+            <button id="submitEdit" class="ui-btn ui-corner-all">LISA</button>
+          </form>
+        </div>
+        <div class="ui-footer ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="footer"
+          role="banner">
+          <h1 class="ui-title" tabindex="0" role="heading" aria-level="1">DTI Ruumihaldus</h1>
+        </div>
+      </div>
+
+      <!-- EDIT CORIDOR PAGE -->
+
+      <div data-role="page" id="editCoridor">
+        <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header"
+          role="banner">
+          <h1 class="ui-title" tabindex="0" role="heading" aria-level="1">DTI Ruumihaldus</h1>
+        </div>
+        <div data-role="navbar">
+          <ul>
+            <li><a href="#rooms" data-transition="none" data-icon="bars">Ruumid</a></li>
+            <li><a href="#coridors" data-transition="none" data-icon="bars">Koridorid</a></li>
+            <li><a href="#addRoom" data-transition="none" data-icon="plus">Lisa ruum</a></li>
+            <li><a href="#addCoridor" data-transition="none" data-icon="plus">Lisa koridor</a></li>
+          </ul>
+        </div>
+        <div data-role="content">
+          <form id="editForm">
+            <label for="editCoridorCoordinates">Sisesta koridori koordinaadid: </label>
+            <input type="text" id="editCoridorCoordinates">
+
+            <label for="editCoridorFloor">Sisesta korrus: </label>
+            <input type="text" id="editCoridorFloor">
+
+            <button id="submitEdit" class="ui-btn ui-corner-all">LISA</button>
+          </form>
+        </div>
+        <div class="ui-footer ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="footer"
+          role="banner">
+          <h1 class="ui-title" tabindex="0" role="heading" aria-level="1">DTI Ruumihaldus</h1>
+        </div>
+      </div>
+    </body>
+
+    </html>
