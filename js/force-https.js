4df575e9756b85c5350808bba9c5a244537ffd88
Madis <Madis0@users.noreply.github.com>
2019-06-12 10:57:00 +0300

    Force HTTPS on all pages

---
+++ js/force-https.js
@@ -0,0 +1,5 @@
+if (window.location.href.indexOf("greeny.cs.tlu.ee") != -1 || window.location.href.indexOf("www.tlu.ee") != -1) {
+    if (location.protocol == 'http:') {
+        location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+    }
+}
