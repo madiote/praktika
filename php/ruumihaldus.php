2b615ed7f3fe8f2a98d046bdc23d83f51c3fe65d
Karen <43170840+karenminski@users.noreply.github.com>
2019-06-18 11:02:47 +0300

    New user on other file, fixed issues

--- php/ruumihaldus.php
+++ php/ruumihaldus.php
@@ -24,30 +24,30 @@ if (isset($_POST["submitUserData"])) { // Don't check before sending the form
   if (isset($_POST["username"]) and !empty($_POST["username"])) {
     $username = test_input($_POST["username"]);
   } else {
-    $notice += "Palun sisesta oma kasutajanimi!";
+    $usernameError = "Palun sisesta oma kasutajanimi!";
   }
 
   if (isset($_POST["password"]) and !empty($_POST["password"])) {
     $password = test_input($_POST["password"]);
     if (strlen($password) < 8) {
-      $notice += "Palun sisesta piisavalt pikk parool!";
+      $passwordError = "Palun sisesta piisavalt pikk parool!";
     }
   } else {
-    $notice += "Palun sisesta oma parool!";
+    $passwordError = "Palun sisesta oma parool!";
   }
 
   if (isset($_POST["passwordconfirm"]) and !empty($_POST["passwordconfirm"])) {
     $password = test_input($_POST["password"]);
     if ($_POST["passwordconfirm"] != $_POST["password"]) {
-      $notice += "Palun sisesta samad paroolid!";
+      $passwordError2 = "Palun sisesta samad paroolid!";
     }
   } else {
-    $notice += "Palun kinnita ka oma parooli!";
+    $passwordError2 = "Palun kinnita ka oma parooli!";
   }
 
   // Check if errors are empty
   if (empty($usernameError) and empty($passwordError) and empty($passwordError2)) {
-    $notice += signup($username, $_POST["password"]);
+    $notice = signup($username, $_POST["password"]);
   }
 }
 
@@ -66,11 +66,10 @@ if (isset($_POST["submitUserData"])) { // Don't check before sending the form
   <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.css">
   <link rel="stylesheet" href="../css/ruumihaldus.css">
   <script src="https://code.jquery.com/jquery-3.4.1.min.js"
-			  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-			  crossorigin="anonymous"></script>
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
   <script type="text/javascript">
     function reload() {
-      setTimeout(function() {
+      setTimeout(function () {
         location.reload();
       }, 10);
     }
@@ -88,12 +87,14 @@ if (isset($_POST["submitUserData"])) { // Don't check before sending the form
   <!-- RUUMID -->
 
   <div data-role="page" id="rooms">
-    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header" role="banner">
+    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header"
+      role="banner">
       <h1></h1>
     </div>
-    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header" role="banner">
+    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header"
+      role="banner">
       <h1 class="ui-title" tabindex="0" role="heading" aria-level="1"></h1>
-      <a href="ruumihaldus.php#addUsers" data-icon="user">Lisa uus kasutaja</a>
+      <a href="#addUsers" data-icon="user">Lisa uus kasutaja</a>
       <a href="?logout=1" onclick="return reload();" data-icon="power">Logi välja</a>
     </div>
     <div data-role="navbar">
@@ -104,7 +105,8 @@ if (isset($_POST["submitUserData"])) { // Don't check before sending the form
       </ul>
     </div>
     <div data-role="content">
-      <ul id="roomProperties" data-role="listview" data-filter="true" data-filter-placeholder="Otsi ruumi..." data-inset="true"></ul>
+      <ul id="roomProperties" data-role="listview" data-filter="true" data-filter-placeholder="Otsi ruumi..."
+        data-inset="true"></ul>
       <button id="downloadRoomsButton" data-theme="A">Lae ruumid alla</button>
       <button id="deleteRoomsButton" data-theme="A">Kustuta kõik ruumide andmed</button>
       <div>
@@ -112,19 +114,22 @@ if (isset($_POST["submitUserData"])) { // Don't check before sending the form
         <input type="file" data-theme="A" id="uploadRoomsButton">
       </div>
     </div>
-    <div class="ui-footer ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="footer" role="banner">
+    <div class="ui-footer ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="footer"
+      role="banner">
       <h1 class="ui-title" tabindex="0" role="heading" aria-level="1">DTI Ruumihaldus</h1>
     </div>
   </div>
   <!-- CORRIDORS -->
 
   <div data-role="page" id="corridors">
-    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header" role="banner">
+    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header"
+      role="banner">
       <h1></h1>
     </div>
-    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header" role="banner">
+    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header"
+      role="banner">
       <h1 class="ui-title" tabindex="0" role="heading" aria-level="1"></h1>
-      <a href="ruumihaldus.php#addUsers" data-icon="user">Lisa uus kasutaja</a>
+      <a href="#addUsers" data-icon="user">Lisa uus kasutaja</a>
       <a href="?logout=1" onclick="return reload();" data-icon="power">Logi välja</a>
     </div>
     <div data-role="navbar">
@@ -137,19 +142,22 @@ if (isset($_POST["submitUserData"])) { // Don't check before sending the form
     <div class="corridorEditor">
       <iframe src="editor.html" width="100%" height="600"></iframe>
     </div>
-    <div class="ui-footer ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="footer" role="banner">
+    <div class="ui-footer ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="footer"
+      role="banner">
       <h1 class="ui-title" tabindex="0" role="heading" aria-level="1">DTI Ruumihaldus</h1>
     </div>
   </div>
 
   <!-- ADD ROOM PAGE -->
   <div data-role="page" id="addRoom">
-    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header" role="banner">
+    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header"
+      role="banner">
       <h1></h1>
     </div>
-    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header" role="banner">
+    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header"
+      role="banner">
       <h1 class="ui-title" tabindex="0" role="heading" aria-level="1"></h1>
-      <a href="ruumihaldus.php#addUsers" data-icon="user">Lisa uus kasutaja</a>
+      <a href="#addUsers" data-icon="user">Lisa uus kasutaja</a>
       <a href="?logout=1" onclick="return reload();" data-icon="power">Logi välja</a>
     </div>
     <div data-role="navbar">
@@ -162,38 +170,41 @@ if (isset($_POST["submitUserData"])) { // Don't check before sending the form
     <div data-role="content">
       <form id="addForm">
         <label for="addClassCoordinates">Sisesta ruumi koordinaadid: </label>
-        <input type="text" id="addClassCoordinates">
+        <input type="text" id="addClassCoordinates" placeholder="2727.73828125&1781.455078125|2728.23828125&1891.955078125">
 
         <label for="addClassRoom">Sisesta ruumi nimi: </label>
-        <input type="text" id="addClassRoom">
+        <input type="text" id="addClassRoom" placeholder="A427">
 
         <label for="addClassPeople">Sisesta ruumiga seotud isikud: </label>
-        <input type="text" id="addClassPeople">
+        <input type="text" id="addClassPeople" placeholder="Mari Maasikas">
 
         <label for="addClassPurpose">Sisesta ruumi eesmärk: </label>
-        <input type="text" id="addClassPurpose">
+        <input type="text" id="addClassPurpose" placeholder="Kabinet">
 
         <label for="addClassSeats">Sisesta ruumi kohtade arv: </label>
-        <input type="number" id="addClassSeats" min="1" max="500">
+        <input type="number" id="addClassSeats" min="1" max="500" placeholder="35">
 
         <label for="addClassComments">Lisa kommentaare: </label>
-        <input type="text" id="addClassComments">
+        <input type="text" id="addClassComments" placeholder="+372 640 9355">
 
         <button id="submitAddRooms" class="ui-btn ui-corner-all">LISA</button>
       </form>
     </div>
-    <div class="ui-footer ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="footer" role="banner">
+    <div class="ui-footer ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="footer"
+      role="banner">
       <h1 class="ui-title" tabindex="0" role="heading" aria-level="1">DTI Ruumihaldus</h1>
     </div>
   </div>
   <!-- EDIT ROOM PAGE -->
   <div data-role="page" id="editRoomPage">
-    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header" role="banner">
+    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header"
+      role="banner">
       <h1></h1>
     </div>
-    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header" role="banner">
+    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header"
+      role="banner">
       <h1 class="ui-title" tabindex="0" role="heading" aria-level="1"></h1>
-      <a href="ruumihaldus.php#addUsers" data-icon="user">Lisa uus kasutaja</a>
+      <a href="#addUsers" data-icon="user">Lisa uus kasutaja</a>
       <a href="?logout=1" onclick="return reload();" data-icon="power">Logi välja</a>
     </div>
     <div data-role="navbar">
@@ -220,18 +231,21 @@ if (isset($_POST["submitUserData"])) { // Don't check before sending the form
         <button id="submitRoomEdit" class="ui-btn ui-corner-all">MUUDA</button>
       </form>
     </div>
-    <div class="ui-footer ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="footer" role="banner">
+    <div class="ui-footer ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="footer"
+      role="banner">
       <h1 class="ui-title" tabindex="0" role="heading" aria-level="1">DTI Ruumihaldus</h1>
     </div>
   </div>
-  <!-- UUS KASUTAJA -->
+  <!-- NEW USER -->
   <div data-role="page" id="addUsers">
-    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header" role="banner">
+    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header"
+      role="banner">
       <h1></h1>
     </div>
-    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header" role="banner">
+    <div class="ui-header ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="header"
+      role="banner">
       <h1 class="ui-title" tabindex="0" role="heading" aria-level="1"></h1>
-      <a href="ruumihaldus.php#addUsers" data-icon="user">Lisa uus kasutaja</a>
+      <a href="#addUsers" data-transition="none" data-icon="user">Lisa uus kasutaja</a>
       <a href="?logout=1" onclick="return reload();" data-icon="power">Logi välja</a>
     </div>
     <div data-role="navbar">
@@ -241,20 +255,8 @@ if (isset($_POST["submitUserData"])) { // Don't check before sending the form
         <li><a href="#corridors" data-transition="none" data-icon="bars">Koridorid</a></li>
       </ul>
     </div>
-    <div data-role="content">
-      <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        <label>Kasutajanimi: </label>
-        <input type="text" name="username" value="<?php echo $username; ?>"><span><?php echo $usernameError; ?></span>
-        <label>Salasõna: </label>
-        <input type="password" name="password" value=""><span><?php echo $passwordError; ?></span>
-        <label>Salasõna uuesti: </label>
-        <input type="password" name="passwordconfirm" value=""><span><?php echo $passwordError2; ?></span>
-        <input type="submit" name="submitUserData" value="Loo kasutaja" target="_blank">
-        <p><?php echo $notice; ?></p>
-      </form>
-
-
-
+    <div class="corridorEditor">
+      <iframe src="signup.php" width="100%" height="600"></iframe>
     </div>
   </div>
   <div class="ui-footer ui-bar-a" data-swatch="a" data-theme="A" data-form="ui-bar-a" data-role="footer" role="banner">
