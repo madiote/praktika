008c3f86c43bb8aa3fcfc3d02930e3a442f5a948
Madis <Madis0@users.noreply.github.com>
2019-06-19 10:58:25 +0300

    Additional instructions

--- php/editor-instructions.php
+++ php/editor-instructions.php
@@ -10,7 +10,7 @@
 </head>
 
 <body>
-    <h2>Koridorihaldur</h2><br>
+    <h2>Koridorihaldur</h2>
     <p>Koridorihalduri eesmärgiks on võimaldada kaardisüsteemil navigeerida ühest ruumist teise. Haldur aitab hallata
         kahte faili:</p>
     <ol>
@@ -39,6 +39,20 @@
         <li>Tee uuesti - teostab viimati tagasi võetud muudatuse uuesti</li>
         <li>Otsingukast - leiab kasti sisestatud tekstile vastava objekti</li>
     </ol>
+    <p>Haldusakna objektide nupud:</p>
+    <ol>
+        <li>3+3 punktiga nupp - sikutades saab objekte ümber järjestada</li>
+        <li>Akna kujuga nupp - sisaldab alammenüüsid</li>
+        <ol>
+            <li>Tüüp - automaatne, massiiv, objekt või string, üldjuhul pole soovitatav muuta</li>
+            <li>Sorteerimine - võimaldab objekte ümber sorteerida, pole soovitatav kasutada</li>
+            <li>Filtreerimine - võimaldab objekte manipuleerida JMESPath abil, ainult edasijõudnutele</li>
+            <li>Insert - võimaldab uut objekti lisada ja tüüpi valida, lihtsam on kasutada duplikeerimise valikut</li>
+            <li>Duplikeeri - kloonib objekti ja selle alamharusid, mõistlik kasutada uute ruumide lisamisel</li>
+            <li>Eemalda - eemaldab objekti ja selle alamharud</li>
+        </ol>
+        <li>Nool paremale või alla - laienda või pane kokku objekti alamharu</li>
+    </ol>
     <p>Alumised tööriistad:</p>
     <ol>
         <li>Salvesta - salvestab muudetud faili serverisse</li>
@@ -47,8 +61,49 @@
         <li>Vahemaa kalkulaator - sisestades 2 ruumi nime annab tööriist nende vahemaa koordinaadiühikutes, mida saab
             sisestada sisestada navigatsiooni "pathing" faili</li>
     </ol>
-    <br>
-    <h3>
+    <h3>Ruumi lisamine</h3>
+    <ol>
+        <li>Lisa ruum ruumihalduse jaotisest</li>
+        <li>Koridorihaldusest laadi koordinaadid "network"</li>
+        <li>Laienda haru "array" ning keri viimase numbrini</li>
+        <li>Vajuta akna kujuga nupule, vali Duplicate</li>
+        <li>Laienda tekkinud number</li>
+        <li>Määra ruumi nimi (point) ja ukse koordinaadid (cords)</li>
+        <li>Salvesta fail vastava nupuga</li>
+        <li>Laadi navigatsioon "pathing"</li>
+        <li>Laienda haru "object" ning keri viimase numbrini</li>
+        <li>Vajuta akna kujuga nupule, vali Duplicate</li>
+        <li>Laienda tekkinud number</li>
+        <li>Uuenda objekti nimi ruumi nimele vastavaks</li>
+        <li>Objekti harudeks määra ruumile lähimad ruumid ning nende kaugused (kasutades kalkulaatorit)</li>
+        <li>Kui lähimaid ruume on juurde vaja, duplikeeri viimast lähimat ruumi ja uuenda väärtusi vastavalt</li>
+        <li>Salvesta fail vastava nupuga</li>
+    </ol>
+    <h3>Ruumi eemaldamine</h3>
+    <ol>
+        <li>Eemalda ruum ruumihalduse jaotisest</li>
+        <li>Koridorihaldusest laadi koordinaadid "network"</li>
+        <li>Laienda haru "array" ning keri soovitud ruumini</li>
+        <li>Vajuta akna kujuga nupule, vali Remove</li>
+        <li>Salvesta fail vastava nupuga</li>
+        <li>Laadi navigatsioon "pathing"</li>
+        <li>Laienda haru "object" ning keri soovitud ruumini</li>
+        <li>Vajuta akna kujuga nupule, vali Remove</li>
+        <li>Salvesta fail vastava nupuga</li>
+    </ol>
+    <h3>Originaalfaili laadimine</h3>
+    <ol>
+        <li>Vajuta nuppu Laadi originaal</li>
+        <li>Vajadusel muuda</li>
+        <li>Salvesta, soovitud fail kirjutatakse üle (koordinaadid või navigatsioon)</li>
+    </ol>
+    <h3>Vahemaa kalkulaator</h3>
+    <ol>
+        <li>Sisesta esimesse kasti esimese ruumi nimi</li>
+        <li>Sisesta teise kasti teise ruumi nimi</li>
+        <li>Vajuta Arvuta vahemaa, kuvatakse kaugus kahe ruumi vahel</li>
+        <li>Sisesta saadud arv navigatsiooni ruumi alamharusse (vt juhist)</li>
+    </ol>
 </body>
 
 </html>
