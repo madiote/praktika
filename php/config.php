2cedf5412f7dab027610b63f9c7b31d18a0b2d40
Gertin Pakkonen <gertpak@users.noreply.github.com>
2019-06-20 15:21:34 +0300

    moded config.php

--- php/config.php
+++ php/config.php
@@ -5,7 +5,7 @@
 	ini_set('session.use_trans_sid', false);
 
 	$serverHost = "localhost";
-	$serverUsername = "if18";
-	$serverPassword = "ifikas18";
+	$serverUsername = "";
+	$serverPassword = "";
 	$database = "if18_kert_li_1";
 	$loginTable = "Praktika_kasutajad";
